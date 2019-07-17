2faaaebfc5bd8ea7b8b2914feff929c8cccbb227
Dmitry Maksyoma <ledestin@gmail.com>
2019-07-17 23:54:00 +1200

    All-stages draft, no sounds

---
+++ src/Brew.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div v-if="status === 'brewing'">
+      <h1>Brewing...</h1>
+      <img src="./assets/nathan-dumlao-vJY0GVb1SaA-unsplash.jpg" style="width: 400px; height: 400px">
+    </div>
+    <div v-else-if="status === 'removeLeaves'">
+      <h1>Please remove leaves/beans and
+        <button @click="startCooling">proceed to cooling</button>
+      </h1>
+      <img src="./assets/stock-photo-glass-filter-kettle-451220128.jpg" style="width: 400px; height: auto">
+    </div>
+    <div v-else-if="status === 'cooling'">
+      <h1>Cooling down...</h1>
+      <img src="./assets/thought-catalog-OJZB0VUQKKc-unsplash.jpg" style="width: 400px; height: auto">
+    </div>
+    <div v-else-if="status === 'ready'">
+      <h1><button>Stop the bell</button> and enjoy your drink</h1>
+      <img src="./assets/bibarys-ibatolla-Mq0jFE_HDJc-unsplash.jpg" style="width: 400px; height: auto">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'brew',
+    data () {
+      return {
+        status: "brewing"
+      }
+    },
+    methods: {
+      startCooling() {
+        this.status = "cooling"
+
+        setTimeout(() => {
+          this.status = "ready"
+        }, 3000)
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this.status = "removeLeaves"
+      }, 3000)
+    }
+  }
+</script>
---
+++ src/Prepare.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Pour water on leaves and <button @click="nextStep" >Start Brewing</button></h1>
+    <img src="./assets/oriento-SSj5qtnrpj0-unsplash.jpg" style="width: 400px; height: 400px">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'prepare',
+    props: {
+      nextStep: Function
+    }
+  }
+</script>
