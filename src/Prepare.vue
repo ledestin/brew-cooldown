5251c2f0aa0ce5369b967cfec75d0fb5e1ac9e06
Dmitry Maksyoma <ledestin@gmail.com>
2019-07-25 13:30:28 +1200

    Image takes 60% of vertical space

--- src/Prepare.vue
+++ src/Prepare.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-bind="$attrs">
     <h2 class="title">Pour water on leaves and <button class="button is-primary" @click="nextStep">Start Brewing</button></h2>
-    <img src="./assets/rene-pollock-VXIOMNlnSos-unsplash.jpg" style="width: 400px; height: 400px">
+    <img src="./assets/rene-pollock-VXIOMNlnSos-unsplash.jpg"
+      style="object-fit: cover; height: 60vh; width: 400px">
   </div>
 </template>
 
