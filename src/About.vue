5a5e6425fa2f029678c1bfee68d4fc27028dcb22
Dmitry Maksyoma <ledestin@gmail.com>
2020-05-26 18:27:58 +1200

    Add demo video to About page

--- src/About.vue
+++ src/About.vue
@@ -3,5 +3,7 @@
     <p class="title is-1">Brewing hot drinks? ☕</p>
     <p class="title is-2">This app tells you, when your
       drink stops being scalding hot, and when to stop brewing.</p>
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/zFpMJqdTNEk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </template>
